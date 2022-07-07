2597259e9eadab058f0b62ab9e606fa9d0a71202
raulalvesre <raul.alves.re@gmail.com>
2022-07-07 00:05:30 -0300

    fix HasPreviousPage logic

--- WeCare.Domain/Core/Pagination.cs
+++ WeCare.Domain/Core/Pagination.cs
@@ -6,7 +6,7 @@ public class Pagination<T>
     public int PageSize   { get; set; }
     public int TotalCount { get; set; }
     public int TotalPages { get; set; }
-    public bool HasPreviousPage => (PageNumber > 0);
+    public bool HasPreviousPage => (PageNumber > 1);
     public bool HasNextPage => (PageNumber+1 < TotalPages);
     public IEnumerable<T> Data { get; set; }
 
