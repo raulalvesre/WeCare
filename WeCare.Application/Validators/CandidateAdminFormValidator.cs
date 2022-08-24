91f00c28230bf10b945a3d4cf5c8f9aa541ab428
raul.alves.re <raul.alves.re@gmail.com>
2022-08-23 21:45:18 -0300

    sei la

--- WeCare.Application/Validators/CandidateAdminFormValidator.cs
+++ WeCare.Application/Validators/CandidateAdminFormValidator.cs
@@ -14,9 +14,23 @@ public class CandidateAdminFormValidator : AbstractValidator<CandidateAdminForm>
             .WithMessage("O email deve ser válido");
 
         RuleFor(x => x.Password)
+            .NotEmpty()
+            .WithMessage("É necessário uma senha")
             .MinimumLength(6)
             .WithMessage("A senha precisa ter no minínmo 6 caracteres")
             .MaximumLength(500)
             .WithMessage("A senha precisa ter no máximo 500 caracteres");
+
+        //TODO quando manda null n vem a mensagem de null
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("É necessário um nome")
+            .MinimumLength(3)
+            .WithMessage("O nome precisa ter no minímo 3 caracteres")
+            .MaximumLength(500)
+            .WithMessage("O nome pode ter no máximo 500 caracteres");
+
+        RuleFor(x => x.Telephone)
+
     }
 }
